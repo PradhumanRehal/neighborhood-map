12d0393915486b763bb3419bbd0ac4f709276d80
PradhumanRehal <pradhuman.rehal@gmail.com>
2017-08-05 14:15:12 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var model = [
 	},
 	{
 		title: 'second one',
-		location: {lat: 40.741359,lng: -73.99102439},
+		location: {lat: 40.741359,lng: -73.99302439},
 		marker: '',
 		infoWindow: ''
 	},
@@ -22,7 +22,8 @@ var model = [
 
 var MapList = function(data){
 	this.title = ko.observable(data.title);
-	this.location = ko.observable(data.location);	
+	this.location = ko.observable(data.location);
+	this.marker = ko.observable(data.marker);	
 };
 
 var ViewModel = function(){
@@ -31,6 +32,7 @@ var ViewModel = function(){
 	this.mapList = ko.observableArray([]);
 	this.map='';
 	this.default = {lat: 40.741359,lng: -73.99102439999996};
+	this.markers = [];
 
 	model.forEach(function(mapItem){
 		self.mapList.push(new MapList(mapItem));
@@ -45,8 +47,24 @@ var ViewModel = function(){
 			center: self.default,
 			zoom: 15
 		});
+		createMarker();
 	};
 
+	var createMarker = function() {
+		model.forEach(function(data){
+			var marker = new google.maps.Marker({
+				map: self.map,
+				position: data.location,
+				title: data.title,
+				animation: google.maps.Animation.DROP,
+
+			})
+		});	
+	};	
+
+	var populateInfoWindow = function() {
+		
+	}
 
 	this.initialize = initMap();
 };
