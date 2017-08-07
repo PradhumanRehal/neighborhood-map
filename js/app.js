123158f8983933c9eda762a37c0794753e083976
PradhumanRehal <pradhuman.rehal@gmail.com>
2017-08-07 23:40:43 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
 var locations = [
-	{title: 'first one', location: {lat: 40.741359,lng: -73.99202439}, marker: '', selected:false, infoWindow: ''},
-	{title: 'second one', location: {lat: 40.741359,lng: -73.99302439}, marker: '', selected:false, infoWindow: ''},
-	{title: 'third one', location: {lat: 40.741359,lng: -73.99102439}, marker: '', selected:false, infoWindow: ''}
+	{title: 'Delhi Gate (Red Fort)', location: {lat: 28.656159,lng: 77.24102}, marker: '', selected:false, infoWindow: '', wikiData:''},
+	{title: 'Lotus Temple', location: {lat: 28.553492,lng: 77.258826}, marker: '', selected:false, infoWindow: '', wikiData:''},
+	{title: 'India Gate', location: {lat: 28.61094,lng: 77.234482}, marker: '', selected:false, infoWindow: '', wikiData:''},	
+	{title: 'National Zoological Park Delhi', location: {lat: 28.603018,lng: 77.246548}, marker: '', selected:false, infoWindow: '', wikiData:''},
+	{title: 'Qutb Minar', location: {lat: 28.524428,lng: 77.185456}, marker: '', selected:false, infoWindow: '', wikiData:''},
+	{title: 'National Rail Museum, New Delhi', location: {lat: 28.585499,lng: 77.180089}, marker: '', selected:false, infoWindow: '', wikiData:''}
 ];
 
 var MapList = function(data){
 	this.title = ko.observable(data.title);
 	this.location = ko.observable(data.location);
-	this.marker = ko.observable(data.marker);	
+	this.marker = ko.observable(data.marker);
+	this.wikiData = ko.observable(data.wikiData);	
 };
 
 var initMap = function(){
 		return new google.maps.Map(document.getElementById('map'),{
-		center: {lat: 40.741359,lng: -73.99102439999996},
-		zoom: 15
+		center: {lat: 28.613939,lng: 77.209021},
+		zoom: 11
 	});
 };
 
@@ -43,9 +47,9 @@ var ViewModel = function(){
 		self.markers.push(new self.createMarker(location));
 	});
 
-	console.log(self.markers().length);
-
 	self.infowindow = new google.maps.InfoWindow();
+
+
 
 	self.clickListener = function(marker){
 		google.maps.event.addListener(marker,'click',function(){
@@ -53,10 +57,31 @@ var ViewModel = function(){
 		});
 	};
 
-	self.populateInfoWindow = function(marker) {
+	self.getWikiData = function(){
+		var wikiQuery;
+
+		for(var i=0; i<self.markers().length; i++){
+			wikiQuery = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + self.markers()[i].title + '&srproperties=snippet&format=json&callback=wikiCallback';
+			
+			$.ajax({
+				url: wikiQuery,
+				dataType:'jsonp',
+				success: function(data) {
+					for(var i=0; i<self.markers().length; i++){
+						if(data[1][0] == self.markers()[i].title){
+							self.markers()[i].wikiData=data[2][0];
+						}
+					}
+				}
+			});			
+		}
+	};
+
+	self.populateInfoWindow = function(marker) { 
+		self.getWikiData();
 		if(self.infowindow.marker != marker){
 			self.infowindow.marker = marker;
-			self.infowindow.setContent(marker.title);
+			self.infowindow.setContent(marker.title+"wikidata here"+marker.wikiData);
 			self.infowindow.open(map,marker);
 			self.infowindow.addListener('closeclick',function(){
             self.infowindow.marker = null;
